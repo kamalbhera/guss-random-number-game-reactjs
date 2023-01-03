84f0890a8863f1194c109b9f933cdc7e3841a777
Kamal Bhera <kamalbhera96@gmail.com>
2023-01-04 00:41:34 +0530

    initiall commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [number, setNumber] = useState('');
+  const [result, setResult] = useState('');
+  const hendelClick = () => {
+    let random = Math.floor(Math.random() * 10 + 1);
+    console.log('====================================');
+    console.log(random);
+    console.log('====================================');
+    if (number !== "") {
+      if (random === Number(number)) {
+        setResult("Yay!! You Guessed it right 🥳");
+      } else {
+        setResult("Oops!! Wrong Guess 😬");
+      }
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +26,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h5>
+          Here you need to guess a number between 1 to 10 <br />
+          If your guessed number matches with the random number generated <br />
+          then you win or else you lose
+        </h5>
+        <input type={'number'} className="randomNumber" name="randomNumber" onChange={(e) => setNumber(e.target.value)} value={number} />
+        <button className='styled' type='button' onClick={hendelClick}>Check</button>
+        <p>{result}</p>
       </header>
     </div>
   );
